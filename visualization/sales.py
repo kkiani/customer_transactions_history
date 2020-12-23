1013efe2945935a9cecf0df5917f9f8e9eda4ad3
Kiarash Kiani <hi@kiarash.info>
2020-12-23 10:51:25 +0330

    init visuals files for productions

---
+++ visualization/sales.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+import calendar
+from dateutil.relativedelta import *
+
+import warnings
+warnings.filterwarnings('ignore')
