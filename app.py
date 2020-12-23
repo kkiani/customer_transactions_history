75352f7c39b57b64763f74eb627f54ab4bc06229
Kiarash Kiani <hi@kiarash.info>
2020-12-23 10:56:45 +0330

    update commands name for better clearification

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ from models import predict_model, train_model
 
 app = typer.Typer()
 
-@app.command('ct')
+@app.command(help='Create an ordered (descending) plot that shows the total number of transactions per customer from the most active customer to the least active one.')
 def customer_transactions():
     typer.echo('ok')
 
-@app.command('pt', help='Products Transaction frequency per month for the year 2018')
+@app.command(help='Given any product ID, create a plot to show its transaction frequency per month for the year 2018.')
 def product_transactions(product_id: int):
     typer.echo('f')
 
-@app.command('train', help='Train the model for prediction.')
+@app.command(help='Train the model for prediction.')
 def train(month: int, epochs: int=50):
     typer.secho(f'🍻 You are training the model with month={month}, make sure to pass same month in prediction', fg=typer.colors.MAGENTA)
     train_model.train(selected_month=month, epochs=epochs)
 
-@app.command('predict', help='Predict the total number of transactions for the next three months per customer anywhere in 2019')
+@app.command( help='Predict the total number of transactions for the next three months per customer anywhere in 2019')
 def predict(customer_id: int, month: int):
     predict_model.predict(customer_id, month)
 
