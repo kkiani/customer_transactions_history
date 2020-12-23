b0c3bb65af1322709f2062f1f48b3c2f8a91e32e
Kiarash Kiani <hi@kiarash.info>
2020-12-23 10:42:06 +0330

    changed to load from processed data

--- models/train_model.py
+++ models/train_model.py
@@ -2,10 +2,13 @@ import argparse
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
+import typer
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+import joblib
 
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, LSTM
@@ -13,26 +16,27 @@ from tensorflow.keras.layers import Dense, LSTM
 import warnings
 warnings.filterwarnings('ignore')
 
-def train(selected_month=1):
+def train(selected_month: int, epochs: int):
     # loading dataset
-    frames = [pd.read_csv('data/transactions_1.csv', index_col=0), pd.read_csv('data/transactions_2.csv', index_col=0)]
-    df = pd.concat(frames)
-    df['date'] = pd.to_datetime(df['date'])
+    df = pd.read_csv('data/trans_per_month.csv', index_col='customer_id')
 
     # calculating product frequency  per months
-    prediction_start_date = f'2019-{selected_month}-01'
-    prediction_end_date = f'2019-{selected_month+3}-01'
-    transactions_per_month_df = df.groupby(['customer_id', pd.Grouper(freq='M', key='date', closed='left')])['customer_id'].count().unstack(fill_value=0)
+    prediction_start_date = str(datetime.date(2019, selected_month, 1))
+    prediction_end_date = str(datetime.date(2019, selected_month+3, 1)) 
 
     # selecting data
-    X = transactions_per_month_df.loc[:, transactions_per_month_df.columns.to_series().between('2017-01-01', prediction_start_date)].values
-    y = transactions_per_month_df.loc[:, transactions_per_month_df.columns.to_series().between(prediction_start_date, prediction_end_date)].sum(axis=1).values
+    X = df.loc[:, df.columns.to_series().between('2017-01-01', prediction_start_date)].values
+    y = df.loc[:, df.columns.to_series().between(prediction_start_date, prediction_end_date)].sum(axis=1).values
 
     # normalizing data
     x_scaler = MinMaxScaler()
     y_scaler = MinMaxScaler()
     X = x_scaler.fit_transform(X)
     y = y_scaler.fit_transform(y.reshape(-1, 1))[:, 0]
+
+    # saving scalers
+    joblib.dump(x_scaler, 'models/serialized/x_scaler.mod') 
+    joblib.dump(y_scaler, 'models/serialized/y_scaler.mod')
 
     # spliting data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=41)
@@ -51,7 +55,7 @@ def train(selected_month=1):
     model.summary()
 
     # training model
-    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, verbose=0)
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, verbose=0)
 
     # saveing model
     model.save('models/serialized/lstm_model')
@@ -69,13 +73,13 @@ def train(selected_month=1):
 
     # calculate root mean squared error
     trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-    print('Train Score: %.2f RMSE' % (trainScore))
+    typer.secho(f'🍻 Train Score: {trainScore:.2f} RMSE', fg=typer.colors.BRIGHT_GREEN)
     testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-    print('Test Score: %.2f RMSE' % (testScore))
+    typer.secho(f'🍻 Test Score: {testScore:.2f} RMSE', fg=typer.colors.BRIGHT_GREEN)
 
     # ploting
     plt.plot(history.history['loss'], label='train')
     plt.plot(history.history['val_loss'], label='validation')
-    plt.title('Model loss with 100 epoch')
+    plt.title(f'Model loss with {epochs} epoch')
     plt.legend()
     plt.show()
